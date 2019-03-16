717a02fb0f0c00f080da13c4bd1dd7f923591efb
Mark Lockett <mark@deliveryengine.net>
2019-03-16 13:43:45 +0800

    Add bad word functionality

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,5 +1,11 @@
 // src/js/store/index.js
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
-const store = createStore(rootReducer);
+import { forbiddenWordsMiddleware } from "../middleware";
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(forbiddenWordsMiddleware)
+);
+
 export default store;
